3a0a625e16dbb61e5470a208558f532142d9485c
maks231107 <maks231107@gmail.com>
2025-09-14 02:22:46 +0300

    Gun2

---
+++ schema/items/weapons/sh_arc9_gekolt_remix_g11.lua
@@ -0,0 +1,7 @@
+ITEM.name = "Husellisch-11"
+ITEM.description = ""
+ITEM.model = "models/weapons/geckololt_css/remix/c_g11.mdl"
+ITEM.class = "arc9_gekolt_remix_g11"
+ITEM.weaponCategory = "Primary"
+ITEM.width = 4
+ITEM.height = 2
---
+++ schema/items/weapons/sh_arc9_gekolt_remix_g3_b.lua
@@ -0,0 +1,7 @@
+ITEM.name = "Gevehrat-38"
+ITEM.description = ""
+ITEM.model = "models/weapons/geckololt_css/remix/c_g3_b.mdl"
+ITEM.class = "arc9_gekolt_remix_g3_b"
+ITEM.weaponCategory = "Primary"
+ITEM.width = 4
+ITEM.height = 2
