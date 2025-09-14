13697b8f8083e8a66edfe131a4bddd28b4729b7a
dailyweek <vinegar1love@gmail.com>
2025-09-14 03:01:11 +0300

    sex

--- schema/items/weapons/sh_bow.lua
+++ schema/items/weapons/sh_bow.lua
@@ -3,5 +3,5 @@ ITEM.description = ""
 ITEM.model = "models/weapons/geckololt_css/remix/c_bowhl2.mdl"
 ITEM.class = "arc9_gekolt_remix_bowhl2"
 ITEM.weaponCategory = "Special"
-ITEM.width = 4
+ITEM.width = 2
 ITEM.height = 2
