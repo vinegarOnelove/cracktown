69e6ac8f802be1c26bdf4ae9c1a0cf1a36d16540
dailyweek <vinegar1love@gmail.com>
2025-09-15 21:45:46 +0300

    classes

--- schema/classes/sh_citizen_chemist.lua
+++ schema/classes/sh_citizen_chemist.lua
@@ -7,7 +7,32 @@ function CLASS:OnSet(client)
     local character = client:GetCharacter()
 
     if (character) then
+        -- Сохраняем оригинальную модель перед сменой
+        if not character:GetData("originalModel") then
+            character:SetData("originalModel", character:GetModel())
+        end
+        
+        -- Устанавливаем новую модель варщика
         character:SetModel("models/humans/group03/chemsuit.mdl")
+        
+        -- Уведомление о выборе класса
+        client:Notify("Вы выбрали класс Варщик!")
+    end
+end
+
+function CLASS:OnLeave(client)
+    local character = client:GetCharacter()
+
+    if (character) then
+        -- Восстанавливаем оригинальную модель
+        local originalModel = character:GetData("originalModel")
+        if originalModel then
+            character:SetModel(originalModel)
+            character:SetData("originalModel", nil) -- Очищаем данные
+        end
+        
+        -- Уведомление о смене класса
+        client:Notify("Вы сменили класс с Варщика!")
     end
 end
 
--- schema/classes/sh_police_riot.lua
+++ schema/classes/sh_police_riot.lua
@@ -6,6 +6,12 @@ function CLASS:OnSet(client)
     local character = client:GetCharacter()
 
     if (character) then
+        -- Сохраняем оригинальную модель перед сменой
+        if not character:GetData("originalModel") then
+            character:SetData("originalModel", character:GetModel())
+        end
+        
+        -- Устанавливаем новую модель
         character:SetModel("models/arachnit/random/georgian_riot_police/georgian_riot_police_player.mdl")
         
         -- Проверяем, есть ли уже щит в инвентаре
@@ -18,6 +24,28 @@ function CLASS:OnSet(client)
             client:Notify("Вам выдан щит!")
         else
             client:Notify("У вас уже есть щит!")
+        end
+    end
+end
+
+function CLASS:OnLeave(client)
+    local character = client:GetCharacter()
+
+    if (character) then
+        -- Восстанавливаем оригинальную модель
+        local originalModel = character:GetData("originalModel")
+        if originalModel then
+            character:SetModel(originalModel)
+            character:SetData("originalModel", nil) -- Очищаем данные
+        end
+        
+        -- Убираем щит из инвентаря при смене класса
+        local inventory = character:GetInventory()
+        local shield = inventory:HasItem("riot_shield")
+        
+        if shield then
+            shield:Remove()
+            client:Notify("Щит убран из инвентаря!")
         end
     end
 end
